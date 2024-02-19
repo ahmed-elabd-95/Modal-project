f8aacdd5377d4f406d4b8c8c85a67d7b259521ba
ahmedElabd <a.alabd@qoyod.com>
2024-02-19 17:02:16 +0200

    first work with react

---
+++ src/component/ProductCard.tsx
@@ -0,0 +1,14 @@
+interface IProps{
+
+}
+const ProductCard = ({}: IProps) => {
+  return (
+    <div className="border border-black">
+        <span>
+            ProductCard
+        </span>
+    </div>
+  )
+}
+
+export default ProductCard
