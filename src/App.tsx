2b1280538f944ea241268182f97cc0dc9a8dd9ed
ahmedElabd <a.alabd@qoyod.com>
2024-02-21 15:14:47 +0200

    custom model component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
+import { useState } from "react"
 import ProductCard from "./component/ProductCard"
+import Modal from "./component/UI/Modal"
 import { productList } from "./data"
+import Button from "./component/UI/Button"
 
 const App = () => {
-  const renderProductList = productList.map(product =><ProductCard  key={product.id} product={product}/> )
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  const renderProductList = productList.map(product => <ProductCard key={product.id} product={product} />)
   return (
     <main className="container">
+      <Button onClick={(openModal)} className='bg-blue-800' width='w-fit'>OPEN MODAL</Button>
       <div className=" m-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      {renderProductList}
+        {renderProductList}
       </div>
-        
+
+      <Modal isOpen={isOpen} closeModal={closeModal} title="Edit Products">
+
+        <Button onClick={(closeModal)} className='bg-blue-800' width='w-fit'>SUBMIT</Button>
+      </Modal>
+
+
     </main>
   )
 }
--- src/component/ProductCard.tsx
+++ src/component/ProductCard.tsx
@@ -3,47 +3,37 @@ import { txtcutter } from "../utils/functions"
 import Image from "./Image"
 import Button from "./UI/Button"
 interface IProps {
-product: IProduct
+  product: IProduct
 }
-const ProductCard = ({product }: IProps) => {
-  const {title, description, imageURL, price, category}  = product
+const ProductCard = ({ product }: IProps) => {
+  const { title, description, imageURL, price, category } = product
   return (
     <div className="border rounded-md p-2 flex flex-col max-w-sm md:max-w-lg mx-auto">
-      
-      <Image 
-      imageURL={imageURL}
-      alt={category.name}
-      className="rounded-md h-52 w-full lg:object-cover"
+
+      <Image
+        imageURL={imageURL}
+        alt={category.name}
+        className="rounded-md h-52 w-full lg:object-cover"
       />
-      
       <h3 className="text-lg font-semibold">{txtcutter(title, 20)}</h3>
-
-
-      <p>
-       
-       <p className="text-xs text-gray-500 break-words">{txtcutter(description)}</p>
-
-      </p>
-
+      <p className="text-xs text-gray-500 break-words">{txtcutter(description)}</p>
       <div className="flex item-center my-4 space-x-2">
         <span className="w-5 h-5 bg-indigo-600 rounded-full cursor-pointer" />
         <span className="w-5 h-5 bg-red-600 rounded-full cursor-pointer" />
         <span className="w-5 h-5 bg-green-600 rounded-full cursor-pointer" />
         <span className="w-5 h-5 bg-yellow-600 rounded-full cursor-pointer" />
       </div>
-
       <div className="flex items-center justify-between">
-        <span className="text-lg text-indigo-600 font-semibold">${price}</span>        
-        <Image 
-         imageURL={category.imageURL}
-         alt={category.name}
-         className="w-10 h-10 rounded-full object-bottom"
+        <span className="text-lg text-indigo-600 font-semibold">${price}</span>
+        <Image
+          imageURL={category.imageURL}
+          alt={category.name}
+          className="w-10 h-10 rounded-full object-bottom"
         />
       </div>
-
       <div className="flex items-center justify-between space-x-2 mt-5">
-        <Button className= "bg-blue-800">EDIT</Button>
-        <Button className= "bg-red-800">DELETE</Button>
+        <Button className="bg-blue-800">EDIT</Button>
+        <Button className="bg-red-800">DELETE</Button>
       </div>
     </div>
   )
