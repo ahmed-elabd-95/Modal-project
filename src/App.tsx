dd43c00c2d836d11f01e6e04a59c330863d21ed2
ahmedElabd <a.alabd@qoyod.com>
2024-02-22 14:57:41 +0200

    add validation

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,10 @@ import { formInputsList, productList } from "./data"
 import Button from "./component/UI/Button"
 import Input from "./component/UI/Input"
 import { IProduct } from "./interfaces"
+import { productValidation } from "./validation"
 
 const App = () => {
-
-
-  const defaultProductObj ={
+  const defaultProductObj = {
     title: '',
     description: '',
     imageURL: '',
@@ -28,30 +27,34 @@ const App = () => {
   const openModal = () => setIsOpen(true);
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     const { value, name } = event.target
-
     setProduct({
       ...product,
       [name]: value,
     });
   }
-
-  const submitHundler = (event: FormEvent<HTMLFormElement>): void  => {
-    event.preventDefault();
-  }
-
-  const onCancel = () =>{
+  const onCancel = () => {
     setProduct(defaultProductObj)
     closeModal()
   }
-
-    /* ------ RENDERS ------- */
+  const submitHundler = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    const { title, description, price, imageURL } = product
+    const errors = productValidation({
+      title,
+      description,
+      price,
+      imageURL,
+    })
+    console.log(errors)
+  }
+  /* ------ RENDERS ------- */
   const renderProductList = productList.map(product => <ProductCard key={product.id} product={product} />)
   const renderFormInputList = formInputsList.map(input => <div className="flex flex-col" key={input.id}>
     <label htmlFor={input.id} className="mb-[2px] text-sm font-medium text-gray-700">{input.label}</label>
     <Input type="text" id={input.id} name={input.name} value={product[input.name]} onChange={onChangeHandler} />
   </div>)
 
-  
+
   return (
     <main className="container">
       <Button onClick={(openModal)} className='bg-indigo-800' width='w-fit'>OPEN MODAL</Button>
@@ -63,14 +66,12 @@ const App = () => {
         <form className="space-y-3" onSubmit={submitHundler}>
           {renderFormInputList}
           <div className="flex items-center space-x-3">
-            <Button  className='bg-indigo-700 hover:bg-indigo-700'>SUBMIT</Button>
+            <Button className='bg-indigo-700 hover:bg-indigo-700'>SUBMIT</Button>
             <Button onClick={onCancel} className='bg-gray-400' >CANCEL</Button>
           </div>
         </form>
 
       </Modal>
-
-
     </main>
   )
 }
